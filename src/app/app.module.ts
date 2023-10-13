b9cd31deeae8a07f68dbf7569bce2b13970c7b5d
DonKiwi-cyber <106345550+DonKiwi-cyber@users.noreply.github.com>
2023-10-13 09:24:04 -0600

    save changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StddevComponent } from './stddev/stddev.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StddevComponent
   ],
   imports: [
     BrowserModule
